ad709ee64735cf76f38ec3afbfef703fc8c91747
Marcos Mantovani <marcospiderplays@gmail.com>
2024-04-16 21:24:03 -0300

    feat: loader and follower system

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -5,7 +5,21 @@ from .models import UserAccount
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'name', 'email', 'is_staff']
+    model = UserAccount
+    list_display = ['username', 'name', 'email', 'id', 'is_staff']
 
-admin.site.register(UserAccount, CustomUserAdmin)
+    fieldsets = (
+        (None, {'fields': ('username', 'name', 'email', 'password')}),
+        ('Profile', {'fields': ('follows', 'get_followed_by',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    readonly_fields = ('get_followed_by',)
+
+    def get_followed_by(self, obj):
+        return ", ".join([user.username for user in obj.followed_by.all()])
+    get_followed_by.short_description = 'Followed by'
+
 admin.site.unregister(Group)
+admin.site.register(UserAccount, CustomUserAdmin)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
+from django.db.models.signals import post_save
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, name, username, password=None):
+    def create_user(self, email, name, username, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have as email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, username=username)
+        user = self.model(email=email, name=name, username=username, **extra_fields)
 
         user.set_password(password)
-        user.save()
+        user.save(using=self._db) # Se houver algum erro, alterar para sem parametro
 
         return user
-    
+
     def create_superuser(self, email, name, username, password=None):
         user = self.create_user(email, name, username, password)
         user.is_superuser = True
@@ -30,6 +32,12 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    follows = models.ManyToManyField("self",
+                                    related_name="followed_by",
+                                    symmetrical=False,
+                                    blank=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     objects = UserAccountManager()
 
@@ -47,10 +55,3 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
-
-class Profile(models.Model):
-    user = models.OneToOneField(UserAccount, on_delete=models.CASCADE)
-    follows = models.ManyToManyField("self",
-                                    related_name="followed_by",
-                                    symmetrical=False,
-                                    blank=True)
