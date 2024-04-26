7fa0a290654906169e6f1d8a5564cf09769f3bd4
Marcos Mantovani <marcospiderplays@gmail.com>
2024-04-26 14:39:37 -0300

    feat: changing website icon

--- backend/textly/settings.py
+++ backend/textly/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'textlywebsite@gmail.com'
-EMAIL_HOST_PASSWORD = 'vrsxaucbwpjjwedr'
+EMAIL_HOST_USER = getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
